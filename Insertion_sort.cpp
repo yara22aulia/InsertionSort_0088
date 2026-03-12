25918ca2185714e68729e083c8afbafbe3744700
yara22aulia <yara22aulia@gmail.com>
2026-03-12 19:37:04 +0700

    membuat struktur program

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int arr[20];        //membuat array dengan panjang data 20
 int n;              //membuat variable inputan n
 
-void input ()
+void input (
+    
+)
 {      //procedure input
     while (true)
     {
@@ -30,7 +32,7 @@ void input ()
 }
 
 void insertionsort(
-    
+
 )
 {  
     int temp;
