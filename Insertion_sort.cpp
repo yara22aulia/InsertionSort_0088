1f9b3577760b8d02110d60a88dfc772830f91065
yara22aulia <yara22aulia@gmail.com>
2026-03-12 19:05:41 +0700

    menambahkan fungsi display dan melengkapi program utama

---
+++ Insertion_sort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];        //membuat array dengan panjang data 20
+int n;              //membuat variable inputan n
+
+void input (){      //procedure input
+    while (true){
+        cout << "Masukan Jumlah Data Pada Array :"; 
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "============================================";
+    cout << "===========Masukan Element Array============";
+    cout << "============================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" <<(i+1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionsort(){  //procedure insertion sort
+    int temp;
+    int j;
+
+    for ( int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr [j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i << ":";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+
+}
+
+void display(){
+    cout << endl;
+    cout << "\n================";
+    cout << "\nElement Array" << endl;
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main(){
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
